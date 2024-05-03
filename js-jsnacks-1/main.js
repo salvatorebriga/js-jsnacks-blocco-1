74e9a5dc1d42e2396d8540f610c0bf4d04fd0fc4
salvatorebriga <salvo.z2302@gmail.com>
2024-05-03 11:44:20 +0200

    snack 1

--- js-jsnacks-1/main.js
+++ js-jsnacks-1/main.js
@@ -2,3 +2,11 @@
 
 let parola1 = prompt();
 let parola2 = prompt();
+
+if(parola1.length > parola2.length){
+    console.log(parola1);
+}else if(parola1.length < parola2.length){
+    console.log(parola2);
+}else{
+    console.log(parola1 + ' ' + parola2);
+}
