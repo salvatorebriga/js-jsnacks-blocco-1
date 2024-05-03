95784a87afe8e4de4f0581dad84e0262e52f9ab3
salvatorebriga <salvo.z2302@gmail.com>
2024-05-03 13:13:06 +0200

    added isNan

--- js-jsnacks-3/main.js
+++ js-jsnacks-3/main.js
@@ -1,6 +1,12 @@
 "use strict"
 let somma = 0;
+let numero = 0;
 for(let i = 0; i < 10; i++){
-    somma += parseInt(prompt(`inserisci il ${i + 1}* numero`));
+    numero = parseInt(prompt(`inserisci il ${i + 1}* numero, non inserire caratteri!`));
+    if(isNaN(numero)){
+        i--;
+    }else{
+        somma += numero;
+    }
 }
 console.log('Totale: ' + somma);
