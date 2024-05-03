938ddd6560a65f528f9483def6a9eb3663fe86cc
salvatorebriga <salvo.z2302@gmail.com>
2024-05-03 12:51:00 +0200

    fix

--- js-jsnacks-3/main.js
+++ js-jsnacks-3/main.js
@@ -1,6 +1,6 @@
 "use strict"
 let somma = 0;
 for(let i = 0; i < 10; i++){
-    somma += parseInt(prompt(`inserisci il ${i + 1} * numero`));
+    somma += parseInt(prompt(`inserisci il ${i + 1}* numero`));
 }
 console.log('Totale: ' + somma);
